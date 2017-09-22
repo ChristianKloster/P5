a922ee37fe504a1ee3b7dc72dc0efed475c86fc3
Gjoderum <nicolaigjoederum@gmail.com>
2017-09-22 08:27:06 +0200

    Dataloader.py

---
+++ pyScripts/DataLoading.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import numpy as np
+
+def loadSalesFiles(listOfSalesFiles):
+	df = pd.DataFrame()
+	for file in listOfSalesFiles:
+		df = df.append(loadSalesFile(file))
+	return df
+
+def loadSalesFile(filePath):
+	return pd.read_csv(filePath, encoding='utf-8',
+								sep=';', 
+								parse_dates=[0], # parse first column
+								skiprows=1, # skip header
+								comment='(', # ignore the '(n rows affected)' footer
+								names=[
+									'date', 
+									'supplierID', 
+									'retialerID', 
+									'supplierItemgroupID', 
+									'SupplierItemgroupName', 
+									'productID',
+									'styleNumber',
+									'description',
+									'colorname',
+									'isNOS',
+									'size',
+									'quantity',
+									'turnover',
+									'discount' ],
+								dtype={
+									'supplierID':np.int64, 
+									'retialerID':np.int64, 
+									'supplierItemgroupID':np.int64, 
+									'SupplierItemgroupName':np.str_, 
+									'productID':np.int64,
+									'styleNumber':np.str_,
+									'description':np.str_,
+									'colorname':np.str_,
+									'isNOS':np.int64,
+									'size':np.str_,
+									'quantity':np.int64,
+									'turnover':np.float64,
+									'discount':np.float64})
+
+# ad hoc testing:
+
+directory = 'C:/Users/nicol/Desktop/AAU/Sales_20'
+files = ['1608', '1609','1610']
+end = '.rpt'
+
+for x in range(0,len(files)):
+	files[x] = directory + files[x] + end
+
+d = loadSalesFiles(files)
+
+print(d['description'].value_counts())
