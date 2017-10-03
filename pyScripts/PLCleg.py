81720235802753522c485b9efc2e62992c2e4f4f
PatrickBSoerensen <31843883+PatrickBSoerensen@users.noreply.github.com>
2017-10-03 07:07:28 +0200

    PLC kombi
    PLC metoderne er blevet lagt sammen og måden de gemmes på burde være mere overskuelig.

--- pyScripts/PLCleg.py
+++ pyScripts/PLCleg.py
@@ -3,6 +3,35 @@ import pandas as pd
 from Plotter import plc as plc
 import matplotlib.pyplot as plt
 
+def plotplc(retailer = 'alle', generer_fra = 'description', percent_of_interest = 20):
+    d = dl.loadSalesFiles(files)
+    d = d.dropna(axis=0, how='any')
+    d = d[d.isNOS != 1]
+    if retailer != 'alle':
+        d = d[d.retailerID == retailer]
+
+    # Laver en sorteret liste sorteret efter mængde forekomster
+    a = d[generer_fra].value_counts()
+    # Summere og har med overPercentDesc mulighed for at give en procentvis grænse for hvor stor en del af salget
+    # der skal plottes for. 0.01 plotter kun varer der udgør 1% af total salg
+
+    a = a.head(int(round(a.size * (percent_of_interest / 100))))
+    for x in a:
+        tilPLC = d[d[generer_fra] == a[a == x].index[0]]
+        plc(tilPLC, 'PLC/{0}/{1}/{2}'.format(retailer, generer_fra, tilPLC['styleNumber'].iloc[0]))
+
+def productreturn(id, stylenumber = 1):
+    d = dl.loadSalesFiles(files)
+    if stylenumber:
+        d = d[d.styleNumber == id]
+        print(d['colorname'].value_counts())
+    else:
+        d = d[d.productID == id]
+        print(d[d.colorname].iloc[0])
+    print(d.SupplierItemgroupName.iloc[0])
+    print(d.description.iloc[0])
+
+
 directory = 'C:/Users/Patrick/PycharmProjects/untitled/AAU/Sales_20'
 files = ['1606', '1607', '1608', '1609','1610', '1611', '1612',
          '1701', '1702', '1703', '1704', '1705', '1706' , '1707', '1708', '1709']
@@ -11,24 +40,8 @@ end = '.rpt'
 for x in range(0,len(files)):
 	files[x] = directory + files[x] + end
 
-d = dl.loadSalesFiles(files)
-d = d.dropna(axis=0, how='any')
-d = d.query("isNOS != 1")
+# plotplc(3, generer_fra = 'description', percent_of_interest=20)
+# plotplc(42, generer_fra = 'description', percent_of_interest=20)
+plotplc(42, generer_fra = 'SupplierItemgroupName', percent_of_interest=20)
 
-genererFra = 'description'
-a = d[genererFra].value_counts()
-total = a.sum()
-overPercentDesc = 0.01
-overPercentStyle = 0.002
-largerthan = total*overPercentDesc
-toStyle = a > int(round(largerthan))
-a= a[toStyle]
-for x in a:
-    forPLC = d[d[genererFra] ==  a[a == x].index[0]]
-    # ignorer det der er udkommenteret her, det er noget bavl der kunne ende ud i en ikke naiv plotter
-    # total = dDescriptions['quantity'].sum()
-    # forPLC = dDescriptions.groupby(by='description').sum(axis='quantity', numeric_only=True)
-    # toPLC = forPLC['quantity'] > total*overPercentStyle
-    # forPLC = forPLC[toPLC]
-    plc(forPLC, 'Gen;{0} Style;{2}'.format(genererFra, forPLC[genererFra].iloc[0],
-                                             forPLC['styleNumber'].iloc[0]), 'place', 'holder')
+# print(productreturn('F14307084', 1))
--- pyScripts/Plotter.py
+++ pyScripts/Plotter.py
@@ -1,26 +1,51 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import os
 import Meta as mt
 import numpy as np
 from datetime import timedelta
 
-def plc(dataframe, name, ret, prod):
+
+def plc(dataframe, name, retailer = 0, product = 0, ignore_returns = 1):
 
         #Bliver ordnet før denne metode, lige nu er det her til at lave test data
-        #dataframe = dataframe.query("retailerID == @ret")
-        #dataframe = dataframe.query("styleNumber == @prod")
-        #Parametre til metadata, mangler fornuftig converter fra date/time til string
-        # firstdate = dataframe['date'].iloc[0]
-        # lastdate = dataframe['date'].iloc[-1]
+        if retailer != 0:
+                dataframe = dataframe[dataframe.retailerID == retailer]
+        if product != 0:
+                dataframe = dataframe[dataframe.productID == product]
+        #Parametre til metadata, pt. udkommenteret fordi meget af det bliver gjort i navnet
+        # firstdate = str(dataframe['date'].iloc[0])
+        # lastdate = str(dataframe['date'].iloc[-1])
         # retailer = str(dataframe['retailerID'].iloc[0])
         # product = str(dataframe['productID'].iloc[0])
         #Sortering og plot
-        dataframe = dataframe.groupby(by='date').sum(axis='quantity', numeric_only=True)
-        dataframe.plot(y='quantity', style='.')
-        # plt.show()
-        #Gemmer lige nu til png fordi vi gerne vil have metadata, men pdf er bedre til brug i rapporten
-        plt.savefig('%s.png' %name)
-        plt.close()
-        #Kalder metadata metoden og tilføjer info
+        if ignore_returns:
+                dataframe = dataframe[dataframe.quantity >= 0]
+
+        dataframe_quantity = dataframe.groupby(by='date').sum()
+        #Den brokker sig pt over denne linje, når der er fundet ud af hvorfor og der er fixet slet denne kommentar :D
+        dataframe['discountP'] = (- dataframe['discount'] ) / (dataframe['turnover'] - dataframe['discount']) * 100
+        dataframe_discount = dataframe.groupby(by='date').mean()
+        dataframe_turnover = dataframe.groupby(by='date').mean()
+
+        #Sammensætter en ny dataframe kun med dem vi er interesserede i
+        dataframe_plot = pd.concat([dataframe_quantity['quantity'], dataframe_discount['discountP'], dataframe_turnover['turnover']],
+                                   axis = 1, keys = ['quantity', 'discount', 'turnover'])
+        #Fungere ligesom groupby, men med en bestemt frekvens 4D = 4 dage, W = week
+
+        dataframe_weekly = dataframe_plot.resample('W').agg({'quantity' : 'sum', 'discount' : 'mean', 'turnover' : 'mean'})
+        dataframe_weekly.plot(style='.')
+        #Udkommenteret mulighed for at lave 2 y akser, jeg syntes det virkede mere uoverskueligt
+        # plt.figure()
+        # ax = dataframe_weekly.plot(secondary_y=['discount', 'turnover'], style='.')
+        # ax.set_ylabel('quantity')
+        # ax.right_ax.set_ylabel('Percent discount, turnover')
+        #Tjekker for mappen, hvis den ikke findes oprettes den
+        directory = os.path.dirname(name)
+        if not os.path.exists(directory):
+                os.makedirs(directory)
+        plt.savefig('%s.png' % name)
         # mt.addMetaData('%s.png' %name, {'from':firstdate, 'to':lastdate,
         #                                  'retailer':retailer, 'product':product})
+        plt.close()
+
