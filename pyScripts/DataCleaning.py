d44e31b999877e1277153fcbfeacc3fd4622febd
Unknown <chrklo@hotmail.com>
2017-10-12 11:50:05 +0200

    Datacleaning 2.0

--- pyScripts/DataCleaning.py
+++ pyScripts/DataCleaning.py
@@ -3,7 +3,8 @@ import numpy as np
 
 directory = 'C:/git repo/P5/GOFACT_DATA/Sales_201612.rpt'
 
-banned_Words = ['Dummy','MEN - BASIC - DON\'T USE','WOMEN - BASIC - DON\'T USE'] #Tilføj ord?
+fyfy_Ord = ['Dummy','MEN - BASIC - DON\'T USE','WOMEN - BASIC - DON\'T USE'] #Mangler ord?
+#test = [] #For testing
 
 #Dataloader
 df = pd.read_csv(directory, encoding='utf-8',
@@ -41,7 +42,15 @@ df = pd.read_csv(directory, encoding='utf-8',
 									'turnover':np.float64,
 									'discount':np.float64})
 
-for word in banned_Words:
-    df = df[df.SupplierItemgroupName != word]
+def datacleaner(df, parameter, words): #Can handle 0, one or more words. 0 Returns the same dataframe.
+    banned_Words = [].append(words) #converts input to list, even if it already is a list
+    if banned_Words: #False if the list is empty, true if not
+        for word in banned_Words:
+            df[df.parameter != word] #Keeps rows where paramter isn't word.
+    return df
 
-df.to_csv(path_or_buf=r'C:\Users\TheChamp\Desktop\out\file.txt') #To ensure they were removed from the dataframe
+#datacleaner(df, 'ItemGroupName', fyfy_Ord)
+datacleaner(df, 'ItemGroupName', fyfy_Ord).to_csv(path_or_buf=r'C:\Users\TheChamp\Desktop\out\file.txt') #To ensure they were removed from the dataframe
+
+
+
