490e40eafdd648d2cd9701712edadffcb82d1c66
Unknown <chrklo@hotmail.com>
2017-11-01 16:11:03 +0100

    CleaningMethod

--- pyScripts/DataCleaning.py
+++ pyScripts/DataCleaning.py
@@ -4,14 +4,14 @@ import dataloader as dl
 from retailerdata import retailerData as rd
 
 #Open the same the clean file to make changes
-df = dl.load_sales_file(r'C:\Users\Christian\Desktop\Min Git mappe\P5\CleanData\CleanedData.rpt')
+df = dl.load_sales_file(r'C:\Git repo\P5\CleanData\CleanedData.rpt')
 
 #For fresh start
-#df = dl.load_sales_files(dl.get_all_files(r'C:\Users\Christian\Desktop\Min Git mappe\P5\GOFACT_DATA\Sales_20'))
+#df = dl.load_sales_files(dl.get_all_files(r'C:\Git repo\P5\GOFACT_DATA\Sales_20'))
 
 #This method opens the meta file and notes which data has been removed.
 def meta_writer(parameter, word, path):
-    with open(path + 'meta.txt', "a") as myfile:
+    with open(path + r'meta.txt', "a") as myfile:
         myfile.write(parameter +': '+ str(word) + '\n')
 
 #df: unclean data. parameter: Columntype. words: Entries you want to filter out. path: The path to the cleandata folder in your git folder.
@@ -28,16 +28,22 @@ def data_cleaner(df, parameter, words, path): #Can handle 0, one or more words. 
 
     df.to_csv(path_or_buf=path+'CleanedData.rpt', index = False, sep=';',encoding='utf-8')
 
-DisList = df['quantity'].tolist()
-DisList.sort()
-print(DisList[len(DisList)-5])
-
-
 #ALREADY CALLED:
-#data_cleaner(df, 'quantity', [400,-400], r'C:\Users\Christian\Desktop\Min Git mappe\P5\CleanData\\')
+#rddf = rd(r'C:\Git repo\P5\Retailers_w_coords.rpt')
+#retailerID = rddf.get_retailers_from_country('Sweden')
+#meta_writer('Sweden','',r'C:\Git repo\P5\CleanData\\')
+#data_cleaner(df, 'retailerID', retailerID['id'], r'C:\Git repo\P5\CleanData\\')
+#retailerID = rddf.get_retailers_from_country('Holland')
+#meta_writer('Holland', '',r'C:\Git repo\P5\CleanData\\')
+#data_cleaner(df, 'retailerID', retailerID['id'], r'C:\Git repo\P5\CleanData\\')
+#retailerID = rddf.get_retailers_from_country('Norway')
+#meta_writer('Norway', '',r'C:\Git repo\P5\CleanData\\')
+#data_cleaner(df, 'retailerID', retailerID['id'], r'C:\Git repo\P5\CleanData\\')
+#retailerID = rddf.get_retailers_from_country('Finland')
+#meta_writer('Finland', '',r'C:\Git repo\P5\CleanData\\')
+#data_cleaner(df, 'retailerID', retailerID['id'], r'C:\Git repo\P5\CleanData\\')
+#retailerID = rddf.get_retailers_from_country('Germany')
+#meta_writer('Germany', '',r'C:\Git repo\P5\CleanData\\')
+#data_cleaner(df, 'retailerID', retailerID['id'], r'C:\Git repo\P5\CleanData\\')
 
-#rddf = rd(r'C:\Users\Christian\Desktop\Min Git mappe\P5\Retailers_w_coords.rpt')
-#retailerID = rddf.get_retailers_from_country('Denmark')
-#data_cleaner(df, 'retailerID', retailerID['id'], r'C:\Users\Christian\Desktop\Min Git mappe\P5\CleanData\\')
-
-#data_cleaner(df, 'SupplierItemgroupName', ['Dummy','Unknown'], r'C:\Users\Christian\Desktop\Min Git mappe\P5\CleanData\\')
+#data_cleaner(df, 'SupplierItemgroupName', ['Dummy','Unknown'], r'C:\Git repo\P5\CleanData\\')
