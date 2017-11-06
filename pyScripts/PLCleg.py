2e8cc8eea5e7c4e41bb676ac1e8a5a659ada641e
PatrickBSoerensen <31843883+PatrickBSoerensen@users.noreply.github.com>
2017-11-06 16:38:05 +0100

    feature returnes tilføjet

--- pyScripts/PLCleg.py
+++ pyScripts/PLCleg.py
@@ -1,16 +1,18 @@
 import dataloader as dl
 from retailerdata import retailerData as rd
 import pandas as pd
+import numpy as np
 from Plotter import plc as plc
+import datetime
 import matplotlib.pyplot as plt
 
 #Generere en liste af transactions fra suppleret retailer og kigger på en defineret top % af dem, de grupperes efter generer_fra
 def plotplc(retailer = 'alle', generer_fra = 'description', percent_of_interest = 20):
-    rddf = rd('C:/Users/SM-Baerbar/Documents/GitHub/P5/Retailers_w_coords.rpt')
-    d_danish = rddf.get_retailers_from_country('Denmark')
-    d_region = rddf.get_retailers_from_region('Sjælland')
+    # rddf = rd('C:/Users/SM-Baerbar/Documents/GitHub/P5/Retailers_w_coords.rpt')
+    # d_danish = rddf.get_retailers_from_country('Denmark')
+    # d_region = rddf.get_retailers_from_region('Sjælland')
 
-    d = dl.load_sales_files(files)
+    d = dl.load_sales_file('C:/Users/Patrick/PycharmProjects/untitled/CleanData/CleanedData.rpt')
     d = d.dropna(axis=0, how='any')
     d = d[d.isNOS != 1]
     #d = d[d.date.dt.day <=7]
@@ -34,7 +36,7 @@ def plotplc(retailer = 'alle', generer_fra = 'description', percent_of_interest 
 
     #Tager et productID eller et stylenumber og udprinter information om varen
 def productreturn(id, stylenumber = 1):
-    d = dl.load_sales_files(files)
+    d = dl.load_sales_file('C:/Users/Patrick/PycharmProjects/untitled/CleanData/CleanedData.rpt')
     if stylenumber:
         d = d[d.styleNumber == id]
         print(d['colorname'].value_counts())
@@ -44,16 +46,56 @@ def productreturn(id, stylenumber = 1):
     print(d.SupplierItemgroupName.iloc[0])
     print(d.description.iloc[0])
 
-directory = 'C:/Users/SM-Baerbar/Documents/GitHub/P5/GOFACT_DATA/Sales_20'
-files = ['1606', '1607', '1608', '1609','1610', '1611', '1612',
-         '1701', '1702', '1703', '1704', '1705', '1706' , '1707', '1708', '1709']
-end = '.rpt'
+def featureplc(d, id):
+    if type(id) is int:
+        df = d[d.productID == id]
+        df = d[d.styleNumber == df['styleNumber'].iloc[0]]
+    else:
+        df = d[d.styleNumber == id]
+    df = df[df.quantity >= 0]
+    df = df.groupby('date', as_index=False).sum()
+    prev = df.shift(periods = 1)
+    test = df['date'] - prev['date']
+    timedif = pd.DataFrame(columns=['date'], index=test.index)
+    test = test.fillna(value=1)
+    for x in test.index:
+        timedif.iloc[x] = int(test.iloc[x].days)
+    timedif.iloc[0] = 1
+    tempslope = (df['quantity'] - prev['quantity'])/timedif['date']
+    slope = tempslope.reindex(df['date'])
+    for x in tempslope.index:
+        slope.iloc[x] = tempslope.iloc[x]
+    slope = slope.fillna(value=0)
+    return slope
 
-for x in range(0,len(files)):
-	files[x] = directory + files[x] + end
+def featurealder(d, id):
+    if type(id) is int:
+        df = d[d.productID == id]
+    else:
+        df = d[d.styleNumber == id]
+    firstdate = df['date'].iloc[0]
+    lastdate = df['date'].iloc[-1]
+    current = pd.to_datetime('today')
+    lifetime = lastdate - firstdate
+    currenttime = current - firstdate
+    return(lifetime.days, currenttime.days)
+
+df = dl.load_sales_file('C:/Users/Patrick/PycharmProjects/untitled/CleanData/CleanedData.rpt')
+
+
+
+# print(featurealder(df, 10721))
+# print(featurealder(df, 'Z99319B'))
+slope = featureplc(df, 'E02100A')
+print(slope)
+slope = slope.resample('W').sum()
+print(slope)
+plt.figure()
+slope.plot()
+plt.show()
 
 # plotplc(3, generer_fra = 'description', percent_of_interest=20)
 # plotplc(42, generer_fra = 'description', percent_of_interest=20)
-plotplc('alle', percent_of_interest=100)
+# plotplc('alle', generer_fra='styleNumber', percent_of_interest=10)
 
 # print(productreturn('F14307084', 1))
