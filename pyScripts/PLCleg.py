48fb829f5fbabded9f9c853ffcc076aeacd3f4c8
PatrickBSoerensen <31843883+PatrickBSoerensen@users.noreply.github.com>
2017-11-08 09:38:38 +0100

    update til PLCleg
    PLCleg indeholder nogle feature metoder

--- pyScripts/PLCleg.py
+++ pyScripts/PLCleg.py
@@ -35,19 +35,20 @@ def plotplc(retailer = 'alle', generer_fra = 'description', percent_of_interest 
         plc(tilPLC, 'PLC/{0}/{1}/{2}'.format(retailer, generer_fra, tilPLC['styleNumber'].iloc[0]))
 
     #Tager et productID eller et stylenumber og udprinter information om varen
-def productreturn(id, stylenumber = 1):
-    d = dl.load_sales_file('C:/Users/Patrick/PycharmProjects/untitled/CleanData/CleanedData.rpt')
-    if stylenumber:
-        d = d[d.styleNumber == id]
-        print(d['colorname'].value_counts())
-    else:
+def productreturn(d, id):
+    if id in d.productID:
         d = d[d.productID == id]
         print(d[d.colorname].iloc[0])
+    else:
+        d = d[d.styleNumber == id]
+        print(d['colorname'].value_counts())
     print(d.SupplierItemgroupName.iloc[0])
     print(d.description.iloc[0])
+    print(d['retailerID'].value_counts())
 
-def featureplc(d, id):
-    if type(id) is int:
+def featureplcBD(d, id):
+    #Relativt store udslag i hældningen, ikke nær så præcis som CD metoden, kan produceres for dags dato
+    if id in d.productID:
         df = d[d.productID == id]
         df = d[d.styleNumber == df['styleNumber'].iloc[0]]
     else:
@@ -66,36 +67,98 @@ def featureplc(d, id):
     for x in tempslope.index:
         slope.iloc[x] = tempslope.iloc[x]
     slope = slope.fillna(value=0)
+    slope = pd.DataFrame(slope, columns=['slope'])
+    return slope
+
+def featureplcCD(d, id):
+    #Denne giver mindre udslag i hældningerne og burde give et mere korrekt udtryk, men den kan ikke produceres for dags dato
+    if id in d.productID:
+        df = d[d.productID == id]
+        df = d[d.styleNumber == df['styleNumber'].iloc[0]]
+    else:
+        df = d[d.styleNumber == id]
+    df = df[df.quantity >= 0]
+    df = df.groupby('date', as_index=False).sum()
+    prev = df.shift(periods = 1)
+    next = df.shift(periods=-1)
+    test = next['date'] - prev['date']
+    timedif = pd.DataFrame(columns=['date'], index=test.index)
+    test = test.fillna(value=1)
+    for x in test.index:
+        timedif.iloc[x] = int(test.iloc[x].days)
+    timedif.iloc[0] = 1
+    timedif.iloc[-1] = 1
+    tempslope = (next['quantity'] - prev['quantity'])/(2*timedif['date'])
+    slope = tempslope.reindex(df['date'])
+    for x in tempslope.index:
+        slope.iloc[x] = tempslope.iloc[x]
+    slope = slope.fillna(value=0)
+    slope = pd.DataFrame(slope, columns=['slope'])
     return slope
 
 def featurealder(d, id):
-    if type(id) is int:
+    if id in d.productID:
         df = d[d.productID == id]
     else:
         df = d[d.styleNumber == id]
+    df = df[df.quantity >= 0]
     firstdate = df['date'].iloc[0]
-    lastdate = df['date'].iloc[-1]
-    current = pd.to_datetime('today')
-    lifetime = lastdate - firstdate
-    currenttime = current - firstdate
-    return(lifetime.days, currenttime.days)
+    lifetimetemp = pd.DataFrame(columns=['lifetime'], index=df.index)
+    for x in lifetimetemp.index:
+        lifetimetemp.loc[x] = int((df['date'].loc[x] - firstdate).days)
+    lifetime = lifetimetemp.reindex(df['date'])
+    for x in range(0, lifetimetemp.shape[0]):
+        lifetime.iloc[x] = lifetimetemp.iloc[x]
+    today = pd.DataFrame([int((pd.to_datetime('today') - firstdate).days)], index=[pd.to_datetime('today')], columns=['lifetime'])
+    lifetime = pd.concat([lifetime, today])
+    return(lifetime)
+
+def featureacceleration(d, id):
+    if id in d.productID:
+        df = d[d.productID == id]
+    else:
+        df = d[d.styleNumber == id]
+    df = df[df.quantity >= 0]
+    df = df.groupby('date', as_index=False).sum()
+    prev = df.shift(periods=1)
+    test = df['date'] - prev['date']
+    timedif = pd.DataFrame(columns=['date'], index=test.index)
+    test = test.fillna(value=1)
+    for x in test.index:
+        timedif.iloc[x] = int(test.iloc[x].days)
+    timedif.iloc[0] = 1
+    slope = (df['quantity'] - prev['quantity']) / timedif['date']
+    slope = slope.fillna(value=0)
+    slope = pd.DataFrame(slope, columns=['slope'])
+    prev = slope.shift(periods=1)
+    tempaccel = (slope['slope'] - prev['slope'])/timedif['date']
+    acceleration = tempaccel.reindex(slope.index)#pd.DataFrame(slope, columns=['acceleration'])
+    for x in tempaccel.index:
+        acceleration.iloc[x] = tempaccel.iloc[x]
+    acceleration = acceleration.fillna(value=0)
+    return acceleration
+
 
 df = dl.load_sales_file('C:/Users/Patrick/PycharmProjects/untitled/CleanData/CleanedData.rpt')
-
+# df = df[df.retailerID == 4]
 
 
 # print(featurealder(df, 10721))
 # print(featurealder(df, 'Z99319B'))
-slope = featureplc(df, 'E02100A')
-print(slope)
-slope = slope.resample('W').sum()
-print(slope)
-plt.figure()
-slope.plot()
-plt.show()
+# print(featurealder(df, 'E02100A'))
 
-# plotplc(3, generer_fra = 'description', percent_of_interest=20)
-# plotplc(42, generer_fra = 'description', percent_of_interest=20)
-# plotplc('alle', generer_fra='styleNumber', percent_of_interest=10)
-
-# print(productreturn('F14307084', 1))
+# slopeBD = featureplcBD(df, 'E02100A')
+# slopeCD = featureplcCD(df, 'E02100A')
+print(featureacceleration(df, 'E02100A'))
+print(featureacceleration(df, 'Z99319B'))
+print(featureacceleration(df, 'F00001451'))
+print(featureacceleration(df, 'F00001460'))
+# print(slopeBD)
+# print(slopeCD)
+# print(slopeBD-slopeCD)
+# plt.figure()
+# slopeBD.plot()
+# plt.show()
+# plt.figure()
+# slopeCD.plot()
+# plt.show()
