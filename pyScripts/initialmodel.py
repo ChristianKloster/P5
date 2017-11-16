9aaced821e39f470fb43a4ce6064c8ac1a875253
PatrickBSoerensen <31843883+PatrickBSoerensen@users.noreply.github.com>
2017-11-16 21:33:27 +0100

    more baselines

--- pyScripts/initialmodel.py
+++ pyScripts/initialmodel.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import dataloader as dl
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score
+import sklearn.metrics #import mean_squared_error, r2_score
 from statsmodels.tsa.stattools import acf, pacf
 from statsmodels.tsa.stattools import adfuller
 from statsmodels.tsa.arima_model import ARIMA
@@ -14,10 +14,12 @@ def baselinemodeler(targetTrain, featureTrain, testTarget, testFeature):
     target = targetTrain.transpose().fillna(value=0)
     feature = featureTrain.transpose().fillna(value=0)
     columns = testTarget.columns.tolist()
-    results = pd.DataFrame(columns=[columns], index=target.index)
-    resultsrelationactual = pd.DataFrame(columns=[columns], index=target.index)
-    resultsrelationactual = resultsrelationactual.fillna(value=0)
-    results['variance'] = 9999
+    results = pd.DataFrame(columns=[['Rsquare', 'lin_mse']], index=target.index)
+    resultsrelationactual = pd.DataFrame(columns=[columns])
+    # resultsrelationactual = resultsrelationactual.fillna(value=0)
+    # results['Rsquare'] = 9999
+    # results['lin_mse'] = 9999
+    dateindex = testTarget.transpose().index
 
     for product in results.index:
         # Initialize the model class.
@@ -28,22 +30,27 @@ def baselinemodeler(targetTrain, featureTrain, testTarget, testFeature):
         testF = testFeature.loc[product]
         testF = testF.values.reshape(-1,1)
         testT = testTarget.loc[product]
+        testT = testT.values
         lin_model.fit(feat, targ)
         pred = lin_model.predict(testF)
 
         # print("Predictions:", newpred)
         # Compute error between our test predictions and the actual values
-        lin_mse = mean_squared_error(pred, testT)
-        r2 = r2_score(testT, pred)
-        score = lin_model.score(pred, testT)
-        results['variance'].loc[product] = r2
-        dumb = testT-pred[0]
-        resultsrelationactual.loc[product] = resultsrelationactual.loc[product] + dumb #results[columns].loc[product] = dumb
+        r2 = sklearn.metrics.r2_score(testT, pred)
+        # r2 = sklearn.metrics.accuracy_score(testT, pred)
+        # r2 = sklearn.metrics.average_precision_score(testT, pred)
+        # r2 = sklearn.metrics.precision_score(testT, pred)
+        lin_mse = sklearn.metrics.mean_squared_error(testT, pred)
+        results['Rsquare'].loc[product] = r2
+        results['lin_mse'].loc[product] = lin_mse
+        dumb = testT - pred
+        dumb = pd.Series(dumb[0], index=dateindex)
+        resultsrelationactual.append(dumb, ignore_index=True) #resultsrelationactual.loc[product] + dumb
         # lin_model =  tuple(map(lambda features, targets : LinearRegression().fit(features, targets), feature[columns], target[columns]))
         # The coefficients
-        print('Coefficients for {1}: {0}'.format(lin_model.coef_, product))
-    resultsrelationactual[['variance']] = results[['variance']]
-    return  resultsrelationactual
+        # print('Coefficients for {1}: {0}'.format(lin_model.coef_, product))
+
+    return  resultsrelationactual, results
 
 def timemodeler(ts, Id, lag = 21):
     ts_log = np.log(ts)
@@ -138,6 +145,9 @@ def test_stationarity(timeseries, Id):
         dfoutput['Critical Value (%s)' % key] = value
     print(dfoutput)
 
+def lin_modeller(df):
+    pass
+
 def tester():
     pass
 
@@ -147,18 +157,17 @@ patrick_dir = r'C:\Users\Patrick\PycharmProjects\untitled\CleanData\\'
 
 dataframe = dl.load_sales_file(patrick_dir + 'CleanedData.rpt')
 
-dataframe = dataframe[dataframe.retailerID == 2]
-dataframe = dataframe[dataframe.isNOS != 1]
+# dataframe = dataframe[dataframe.retailerID == 2]
+# dataframe = dataframe[dataframe.isNOS != 1]
 generer_fra = 'productID'
 
 # products = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
 
-
 #Different test cases for days
-quantity1D = targetprovider.get_pivot_tables_with_target_values(dataframe, retailerid = 2, days = '1D', on = 'productID')
-quantity2D = targetprovider.get_pivot_tables_with_target_values(dataframe, retailerid = 2, days = '2D', on = 'productID')
-quantity4D = targetprovider.get_pivot_tables_with_target_values(dataframe, retailerid = 2, days = '4D', on = 'productID')
-quantity1W = targetprovider.get_pivot_tables_with_target_values(dataframe, retailerid = 2, days = '1W', on = 'productID')
+quantity1D = targetprovider.get_pivot_tables_with_target_values(dataframe, retailerid = 4, days = '1D', on = generer_fra)
+quantity2D = targetprovider.get_pivot_tables_with_target_values(dataframe, retailerid = 4, days = '2D', on = generer_fra)
+quantity4D = targetprovider.get_pivot_tables_with_target_values(dataframe, retailerid = 4, days = '4D', on = generer_fra)
+quantity1W = targetprovider.get_pivot_tables_with_target_values(dataframe, retailerid = 4, days = '1W', on = generer_fra)
 #Creating train data from cases, use whichever one is wanted for testing
 traintarget = quantity1D[0]
 trainfeature = quantity1D[1]
@@ -171,18 +180,70 @@ testtarget = testtarget.head(int(np.ceil(testtarget.shape[0] * 0.1))).iloc[::-1]
 testfeature = testfeature.head(int(np.ceil(testfeature.shape[0] * 0.1))).iloc[::-1]
 testtarget = testtarget.transpose().fillna(value=0)
 testfeature = testfeature.transpose().fillna(value=0)
-
 # Til at teste på udsnit af dataet
 # columns = testtarget.columns.tolist()
 # traintarget = traintarget.iloc[:, [0,1]]
 # trainfeature = trainfeature.iloc[:, [0,1]]
 # testtarget = testtarget.iloc[[0,1]]
 # testfeature = testfeature.iloc[[0,1]]
+daymodel = baselinemodeler(traintarget, trainfeature, testtarget, testfeature)
+
+#Creating train data from cases, use whichever one is wanted for testing
+traintarget = quantity2D[0]
+trainfeature = quantity2D[1]
+traintarget = traintarget.head(int(np.floor(traintarget.shape[0] * 0.9)))
+trainfeature = trainfeature.head(int(np.floor(trainfeature.shape[0] * 0.9)))
+#Creates the test data for verification
+testtarget = quantity2D[0].iloc[::-1]
+testfeature = quantity2D[1].iloc[::-1]
+testtarget = testtarget.head(int(np.ceil(testtarget.shape[0] * 0.1))).iloc[::-1]
+testfeature = testfeature.head(int(np.ceil(testfeature.shape[0] * 0.1))).iloc[::-1]
+testtarget = testtarget.transpose().fillna(value=0)
+testfeature = testfeature.transpose().fillna(value=0)
+
+twodaymodel = baselinemodeler(traintarget, trainfeature, testtarget, testfeature)
+
+#Creating train data from cases, use whichever one is wanted for testing
+traintarget = quantity4D[0]
+trainfeature = quantity4D[1]
+traintarget = traintarget.head(int(np.floor(traintarget.shape[0] * 0.9)))
+trainfeature = trainfeature.head(int(np.floor(trainfeature.shape[0] * 0.9)))
+#Creates the test data for verification
+testtarget = quantity4D[0].iloc[::-1]
+testfeature = quantity4D[1].iloc[::-1]
+testtarget = testtarget.head(int(np.ceil(testtarget.shape[0] * 0.1))).iloc[::-1]
+testfeature = testfeature.head(int(np.ceil(testfeature.shape[0] * 0.1))).iloc[::-1]
+testtarget = testtarget.transpose().fillna(value=0)
+testfeature = testfeature.transpose().fillna(value=0)
+
+fourdaymodel = baselinemodeler(traintarget, trainfeature, testtarget, testfeature)
+
+#Creating train data from cases, use whichever one is wanted for testing
+traintarget = quantity1W[0]
+trainfeature = quantity1W[1]
+traintarget = traintarget.head(int(np.floor(traintarget.shape[0] * 0.9)))
+trainfeature = trainfeature.head(int(np.floor(trainfeature.shape[0] * 0.9)))
+#Creates the test data for verification
+testtarget = quantity1W[0].iloc[::-1]
+testfeature = quantity1W[1].iloc[::-1]
+testtarget = testtarget.head(int(np.ceil(testtarget.shape[0] * 0.1))).iloc[::-1]
+testfeature = testfeature.head(int(np.ceil(testfeature.shape[0] * 0.1))).iloc[::-1]
+testtarget = testtarget.transpose().fillna(value=0)
+testfeature = testfeature.transpose().fillna(value=0)
+
+weekmodel = baselinemodeler(traintarget, trainfeature, testtarget, testfeature)
+
+variances = daymodel[1]
+variances['RsquareDay'] = variances['Rsquare']
+variances['lin_mseDay'] = variances['lin_mse']
+variances['Rsquare2Day'] = twodaymodel[1]['Rsquare']
+variances['lin_mse2Day'] = twodaymodel[1]['lin_mse']
+# variances['Rsquare4Day'] = fourdaymodel['Rsquare']
+# variances['RsquareWeek'] = weekmodel['Rsquare']
+
+print(variances[['RsquareDay', 'Rsquare2Day','lin_mseDay', 'lin_mse2Day']])
 
 
-newmodel = baselinemodeler(traintarget, trainfeature, testtarget, testfeature)
-
-print(newmodel)
 # print("Predictions:", newpred)
 # Compute error between our test predictions and the actual values
 # lin_mse = mean_squared_error(newpred, test['quantity'])
